3869e72ab73dbc6859c1723b5474d088242e0e03
RJSYE <161940979+RJSYE@users.noreply.github.com>
2024-03-10 21:32:30 +0900

    Update C3-9.py

--- C3-9.py
+++ C3-9.py
@@ -1,47 +1,61 @@
-userid = str(input("회원가입 아이디 입력해라"))
-userid2 = str()
-command = str()
+
+userid = str(input("아이디를 입력해 계정을 생성하세요 : "))
 rank = int()
-ban = str()
-if userid or userid2 == "admin" :
+
+userid2 = str("R_JSYE")
+rank2 = int()
+if userid == "admin" :
     rank = 3
-if userid or userid2 == "moderator" :
-    rank = 2
-if userid or userid2 == "vip" :
-    rank =1
-if userid or userid2 == "banned" :
+elif userid == "mod" :
+    rank =2
+elif userid == "vip" :
+    rank = 1
+elif userid == "banned" :
     rank = -1
-    
 else :
     rank = 0
-if rank > 1 :
-    print("환영합니다  " + userid + "관리자 님")
-    command = str(input())
-if rank > 2 :
-    print("owner mod")
-    command = str(input())
-if rank == -1 :
-    print("응너밴 ㅅㄱ")
+if rank == 1 :
+    print("환영해요 vip")
+elif rank == 2 :
+    print("환영해요 모더레이더")
+elif rank == 3 :
+    print("환영해요 관리자")
+elif rank == -1 :
+    print("당신은 밴되었음으로 기능이 제한되었씁니다.씁.")
 else :
-    print("환영합니다 : " + userid or userid2)
-    a = int(input("숫자를 입력하세요"))
-    b=int(input("두번째 숫자를 입력해라"))
-    c=int(input("세번째나 입력해"))
-    larg=str()
-    if a > b and c :
-        larg=a
-    if b > a and c :
-        larg=b
-    else :
-        larg=c
-    print(larg)
-    
-        
-    
-if command == "resister" :
-    userid2 = str(input("관리자 권한으로 새로운 계정을 개설합니다 id : "))
-if command == "ban" and rank > 2 :
-    ban = str(input("밴대상의 아이디를 입력하세요 :"))
+    print("환영합니다 유저님!")
+if rank > 1 :
+    command = str(input("커맨드를 입력해주세요 : "))
+    if command == "ban" :
+        ban = str(input("밴할 대상의 아이디를 적어주세요 : "))
+        if ban == userid :
+            userid = "banned"
+        if ban == userid2 :
+            userid2 = "banned"
+    elif command == "resister" :
+        ac = str(input("생성할 계정의 아이디를 입력해주세요 : "))
+        userid2 = ac
+if userid2 == "admin" :
+    rank2 = 3
+elif userid2 == "mod" :
+    rank2 = 2
+elif userid2 == "vip":
+    rank2 = 1
+elif userid2 == "banned":
+    rank2 = -1
+else :
+    rank2 = 0
+if rank2 == -1 :
+    print("당신은 밴되었으니 환영안해줘요")
+elif rank2 == 1 :
+    print("환영합니다 vip!")
+elif rank2 == 2 :
+    print("환영합니다 모더레이더터!")
+elif rank2 == 3 :
+    print("환영합니다 관리자투!")
+else :
+    print("환영합니다 유저투!")
+ 
 if str(ban) == str(userid) :
     userid = str("banned")
 if str(ban) == str(userid2) :
